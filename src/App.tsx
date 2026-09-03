2874d68740a33dd3ad9b02a10061f717b30647e8
Saksham Dixit <sakshamdixit1124@gmail.com>
2026-09-02 20:22:25 -0500

    Initial React project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>HackIllinois 2027 Schedule</h1>
+      <p>Ocean-themed event schedule coming soon.</p>
+    </div>
+  );
+}
+
+export default App;
